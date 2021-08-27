4007422a0785ad19f13592f49c4d39d75076755a
zhouxixi <iw19990912@163.com>
2021-08-27 11:27:33 +0800

    新增

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import vue from 'vue'
+import VueRouter from 'vue-router'
+
+//1.安装插件
+Vue.use('VueRouter')
+//2.创建路由对象
+
+const routes = []
+const router = new VueRouter({
+  routes,
+  mode:'history',
+})
+
+//3. 导出
+export default router
